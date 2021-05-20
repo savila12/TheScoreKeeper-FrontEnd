84af24c0855439e085f52921b9e9ea4fcc6d7ad6
Sidney Avila <savila1212@gmail.com>
2021-05-20 17:16:26 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const app = express();// Serve only the static files from the dist directory
 // Replace angular-recipes-frontend with your project name
-app.use(express.static(__dirname + '/dist/angular-recipes-frontend'));
+app.use(express.static(__dirname + '/dist/TheScoreKeeper'));
 app.get('/*', function(req,res) {  // Replace angular-recipes-frontend with your project name
   res.sendFile(path.join(__dirname+'/dist/TheScoreKeeper/index.html'));
 });// Start the app by listening on the default Heroku port
